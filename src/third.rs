2b3d44bf1af9b2506ed07fad91bc9f3ea1bf0978
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-16 06:22:22 +0000

    Update with generics

--- src/third.rs
+++ src/third.rs
@@ -1,9 +1,15 @@
 use std::rc::Rc;
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
-type Link = Option<Rc<Node>>;
-pub struct Node {
-    elem: i32,
-    next: Link,
+type Link<T> = Option<Rc<Node<T>>>;
+pub struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
 }
