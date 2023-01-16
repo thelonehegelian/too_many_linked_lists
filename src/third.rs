a39176e867d1432e9b213b9b1c20bacb63977e8a
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-16 06:36:03 +0000

    Add iterator tests

--- src/third.rs
+++ src/third.rs
@@ -78,4 +78,14 @@ mod test {
         let list = list.tail();
         assert_eq!(list.head(), None);
     }
+
+    // Add iterator tests
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
 }
