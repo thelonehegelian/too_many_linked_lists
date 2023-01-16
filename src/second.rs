e966823ec02c5787edbf6df3742ae2789d317cd2
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-16 04:03:40 +0000

    Update type Link

--- src/second.rs
+++ src/second.rs
@@ -2,26 +2,26 @@
 * A type has a destructor if it implements a trait called Drop.
 */
 
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
 // Link in the first.rs is an enum exactly the same as the Options type,
 // it either retuns a value or nothing
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-pub struct Node {
-    elem: i32,
-    next: Link,
+pub struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
 // static method to create a new list
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
         // a new list is an empty list
         List { head: None }
     }
     // push will mutate the list
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Node {
             elem: elem,
             // This function takes a mutable reference to the head
@@ -33,7 +33,7 @@ impl List {
         self.head = Some(Box::new(new_node));
     }
     // pop the node, return the value, return can be empty
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         // match option { None => None, Some(x) => Some(y) } is the same as option.map(|x| y)
         self.head.take().map(|node| {
             self.head = node.next;
