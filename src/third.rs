c83dd6b0edb3b9d40675baa7c7248877a05dcfbd
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-16 06:33:51 +0000

    Add tests prepend, head, til

--- src/third.rs
+++ src/third.rs
@@ -34,3 +34,19 @@ impl<T> List<T> {
         self.head.as_ref().map(|node| &node.elem)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+}
