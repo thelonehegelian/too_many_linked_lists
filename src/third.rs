5fc559888c8751a433700fe05a698e0029a2de7b
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-16 06:31:15 +0000

    prepend and tail

--- src/third.rs
+++ src/third.rs
@@ -12,4 +12,21 @@ impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem: elem,
+                next: self.head.clone(),
+            })),
+        }
+    }
+    pub fn tail(&self) -> List<T> {
+        // map expects us to return a Y, but here we're returning an Option<Y>.
+        // Thankfully, this is another common Option pattern,
+        // and we can just use and_then to let us return an Option
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
 }
